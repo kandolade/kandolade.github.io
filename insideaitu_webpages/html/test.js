17abd8e9677ed45c1b850e398f79098f747b34c8
kandolade <211223@astanait.edu.kz>
2022-02-02 14:16:01 +0600

    aza

---
+++ insideaitu_webpages/html/test.js
@@ -0,0 +1,7 @@
+var yourYear = prompt("How old are you?");
+var yearToDay = yourYear*365;
+var yearToWeek = yourYear*52;
+var yearToMonth = yourYear*12;
+alert('You have ' + (32850 - yearToDay) + ' days, ' + (4680 - yearToWeek) + ' weeks, ' + 'and ' + (1080 - yearToMonth) + ' months left.')
+
+
